e571f2738401c28ef304c050f71a86e6bcaa1fcd
Ranyodh Singh <rsingh@mirantis.com>
2023-11-20 14:10:01 -0500

    add more information for reset command

--- cmd/reset.go
+++ cmd/reset.go
@@ -10,8 +10,15 @@ import (
 // resetCmd represents the apply command
 func resetCmd() *cobra.Command {
 	cmd := &cobra.Command{
-		Use:     "reset",
-		Short:   "Reset the cluster",
+		Use:   "reset",
+		Short: "Reset the cluster to a clean state",
+		Long: `
+Reset the cluster to a clean state. 
+
+For a cluster with k0s, this will remove traces of k0s from all the hosts.
+For a cluster with kind, it will delete the cluster (same as 'kind delete cluster <CLUSTER NAME>').
+For a cluster with an external Kubernetes provider, this will remove Boundless Operator and all associated resources.
+`,
 		Args:    cobra.NoArgs,
 		PreRunE: actions(loadBlueprint),
 		RunE: func(cmd *cobra.Command, args []string) error {
