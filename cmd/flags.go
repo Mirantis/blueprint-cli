58dd0765dcc2a4e1e601e41436999d39a219708b
Ranyodh Singh <rsingh@mirantis.com>
2023-12-01 12:49:56 -0500

    Deprecated/Renamed flags (--config > --file, --debug > --log-level=(trace, debug, info, error)

--- cmd/flags.go
+++ cmd/flags.go
@@ -3,15 +3,18 @@ package cmd
 const (
 	// DefaultBlueprintFileName represents the default blueprint filename.
 	DefaultBlueprintFileName = "blueprint.yaml"
+
+	// DefaultLogLevel represents the default log level.
+	DefaultLogLevel = "info"
 )
 
 // PersistenceFlags represents configuration pFlags.
 type PersistenceFlags struct {
-	Debug bool
+	LogLevel string
 }
 
 func NewPersistenceFlags() *PersistenceFlags {
 	return &PersistenceFlags{
-		Debug: false,
+		LogLevel: DefaultLogLevel,
 	}
 }
