a3bd670992cbc8f4bcd5a194a25c9e959116254e
Nick Neisen <nwneisen@gmail.com>
2024-09-23 15:27:02 -0600

    review comments

--- pkg/commands/utils.go
+++ pkg/commands/utils.go
@@ -16,32 +16,25 @@ var operatorReleaseUri = "https://github.com/MirantisContainers/blueprint/releas
 // Otherwise, it will be assumed to be a version and the URI will be constructed
 func determineOperatorUri(version string) (string, error) {
 	if version == "latest" {
-		uri, err := url.Parse(fmt.Sprintf(operatorReleaseUri, version))
-		if err != nil {
-			return "", fmt.Errorf("failed to parse uri: %w", err)
-		}
-		return uri.String(), nil
+		return parseUri(version)
 	}
 
 	// Check for a valid semver version
-	re, err := regexp.Compile(constants.SemverRegexWithoutV)
+	regexWithoutV, err := regexp.Compile(constants.SemverRegexWithoutV)
 	if err != nil {
 		return "", fmt.Errorf("failed to compile regex: %w", err)
 	}
-	if re.MatchString(version) {
+	regexWithV, err := regexp.Compile(constants.SemverRegexWithV)
+	if err != nil {
+		return "", fmt.Errorf("failed to compile regex: %w", err)
+	}
+	
+	if regexWithoutV.MatchString(version) {
 		// We'll just add the v in this case and handle it with the same code as below
 		version = fmt.Sprintf("v%s", version)
 	}
-	rev, err := regexp.Compile(constants.SemverRegexWithV)
-	if err != nil {
-		return "", fmt.Errorf("failed to compile regex: %w", err)
-	}
-	if rev.MatchString(version) {
-		uri, err := url.Parse(fmt.Sprintf(operatorReleaseUri, version))
-		if err != nil {
-			return "", fmt.Errorf("failed to parse uri: %w", err)
-		}
-		return uri.String(), nil
+	if regexWithV.MatchString(version) {
+		return parseUri(version)
 	}
 	log.Debug().Msg("Version is not a valid semver version, assuming it is a URI")
 
@@ -53,4 +46,13 @@ func determineOperatorUri(version string) (string, error) {
 	log.Debug().Msg("Version is not a valid URI")
 
 	return "", fmt.Errorf("version is not a valid semver version or URI")
+}
+
+// parseUri parses the URI for the operator release
+func parseUri(version string) (string, error) {
+	uri, err := url.Parse(fmt.Sprintf(operatorReleaseUri, version))
+	if err != nil {
+		return "", fmt.Errorf("failed to parse uri: %w", err)
+	}
+	return uri.String(), nil
 }
