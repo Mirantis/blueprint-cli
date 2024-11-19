9a6424c5800a7f117f6f6a2e54bddd3701e9fd47
Raunak Pradip Shah <raunakshah13@gmail.com>
2024-11-19 12:58:28 -0800

    Wait for pods to be ready after bctl upgrade

--- pkg/commands/upgrade.go
+++ pkg/commands/upgrade.go
@@ -2,6 +2,7 @@ package commands
 
 import (
 	"fmt"
+	"github.com/mirantiscontainers/boundless-cli/pkg/distro"
 
 	"github.com/mirantiscontainers/boundless-cli/pkg/k8s"
 	"github.com/mirantiscontainers/boundless-cli/pkg/types"
@@ -21,5 +22,15 @@ func Upgrade(blueprint *types.Blueprint, kubeConfig *k8s.KubeConfig) error {
 	}
 
 	log.Info().Msgf("Finished updating Blueprint Operator")
+
+	// Determine the distro
+	provider, err := distro.GetProvider(blueprint, kubeConfig)
+	if err != nil {
+		return fmt.Errorf("failed to determine kubernetes provider: %w", err)
+	}
+	// Wait for the pods to be ready
+	if err := provider.WaitForPods(); err != nil {
+		return fmt.Errorf("failed to wait for pods: %w", err)
+	}
 	return nil
 }
